eea78bf84e4e3b08548465621b74903f4908b52f
takuya yokoyama <ignisrt7mssp@gmail.com>
2022-08-13 21:37:06 +0900

    Assetsデータ追加

---
+++ upmTest/Assets/Scripts/Common/GlobalObjectHolder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class GlobalObjectHolder : MonoBehaviour
+{
+	const string objectName = "GlobalObjects";
+
+	static GlobalObjectHolder instance;
+	/// <summary>
+	/// インスタンスを取得
+	/// </summary>
+	public static GlobalObjectHolder Instance
+	{
+		get
+		{
+			if(instance == null)
+			{
+				//無い場合は自身を作成する
+				var go = new GameObject(objectName, typeof(GlobalObjectHolder));
+				instance = go.GetComponent<GlobalObjectHolder>();
+			}
+			return instance;
+		}
+	}
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
+	private void OnDestroy()
+	{
+		if(instance == this)
+		{
+			instance = null;
+		}
+	}
+
+	public void Add(GameObject gameObject)
+	{
+		gameObject.transform.SetParent(this.transform, false);
+	}
+}
+
---
+++ upmTest/Assets/Scripts/Common/StartupSceneInfo.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "scenesInfomation", menuName = "ScriptableObjects/CreateSceneInfomation")]
+public class StartupSceneInfo : ScriptableObject
+{
+    [SerializeField]
+    [Tooltip("デフォルトシーン名")]
+    string startupSceneName;
+
+    [SerializeField]
+    [Tooltip("デフォルトシーンへ遷移させるシーンディレクトリ")]
+    string defaultTransitionSceneDirectory = "Scenes";
+
+    [SerializeField]
+    [Tooltip("例外となるシーンディレクトリ名")]
+    string[] excludedFromSceneTransitionNames;
+
+    /// <summary>デフォルトで遷移するシーン名</summary>
+    public string StartupSceneName { get { return startupSceneName; } }
+
+    /// <summary>
+    /// 指定したpathのディレクトリリストを取得
+    /// </summary>
+    void GetDirectoryList(string path, out string[] directories)
+    {
+        var directory = Path.GetDirectoryName(path);
+        char[] seps = { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar };
+
+        //ルートディレクトリを取り除いたものを返す
+        directories = directory.Split(seps).Where(t => t != "Assets").ToArray();
+    }
+
+    /// <summary>
+    /// デフォルトのシーンへ遷移させるかどうかを確認する
+    /// </summary>
+    public bool IsTransitionToTheDefaultScene(string activeSceneFullPath)
+    {
+        var scenes = EditorBuildSettings.scenes;
+
+        string name = Path.GetDirectoryName(activeSceneFullPath);
+        string[] directories = null;
+        GetDirectoryList(activeSceneFullPath, out directories);
+
+        //エディタ上での作成されたが、保存されていないシーン
+        if(activeSceneFullPath =="")
+        {
+            return false;
+		}
+
+        //デフォルト遷移シーン名のnullチェック
+        if(string.IsNullOrEmpty(defaultTransitionSceneDirectory))
+        {
+            return false;
+		}
+
+        if(excludedFromSceneTransitionNames != null && excludedFromSceneTransitionNames.Length > 0)
+        {
+            //例外的なシーンかどうか
+            if (!excludedFromSceneTransitionNames.Any(a => directories.Any(b => a == b)))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
